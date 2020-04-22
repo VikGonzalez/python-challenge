f0bd12a76e95d086dc67331f29606ce166652006
VikGonzalez <61802019+VikGonzalez@users.noreply.github.com>
2020-04-22 14:47:59 -0500

    Update for PyBank Homework
    I´ve made an update for my homework PyBank code.
    
    Regards.

--- python-challenge/PyBank/main.py
+++ python-challenge/PyBank/main.py
@@ -5,52 +5,35 @@ csvpath = os.path.join("Resources", "budget_data.csv")
 with open(csvpath) as csvfile:
     csvreader = csv.reader(csvfile, delimiter = ',')
 
-    print("Financial Analysis")
-    print("--------------------------------------")
     change=[]
     change2=[]
     date=[]
 
     next(csvreader)
-    x = 0
-    y = 0
+    months = 0
+    amount = 0
 
     for row in csvreader:
-        x = x+1
-        y = y + 1 
-        z = (row[1])
+        months = months+1
+        amount = amount + float(row[1])
+        z = float(row[1])
         change.append(z)
-        a= str(row[0])
-        date.append(a)
+        x= str(row[0])
+        date.append(x)
 
-    for i in range(x-1):
-        q = change[i+1]= change[i]
-        change2.append(q)
+    for counter in range (months-1):
+        y= change[counter+1]-change[counter]
+        change2.append(y)
         avg = sum(change2)/len(change2)
+        b= max(change2)
+        c = date[counter+1]
+        d= min(change2)
+        e = date[counter+1]
 
-        b = max(change2)
-
-        for i in range(x-1):
-            if change2[i] ==b:
-                c = date[i+1]
-
-        d = min(change2)
-
-        for i in range(x-1):
-            if change2[i] == d:
-                    e = date[i+1]
-
-print(f"Total Months: {x}")
-print(f"Total: {y}")
-print(f"Average Change: {avg}")
-print(f"Greatest increase in profits: {c} {b}")
-print(f"Greatest decrease in profits: {e} {d}")
-
-
-
-
-
-
-
-
-    
+    print("Financial Analysis")
+    print ("--------------------------------------")
+    print(f"Total Months: {months}")
+    print(f"Total: {amount}")
+    print(f"Average Change: {avg}")
+    print(f"Greatest Increase in Profits: {c} {b} ")
+    print(f"Greatest Increase in Profits: {e} {d} ")
